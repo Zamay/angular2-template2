bcd5010905ae6bfca770dc45fa3811fb1448783a
zaamy <mityazamay@gmail.com>
2017-06-10 12:17:16 +0300

    Маршрутизация . RouterModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,21 @@
-// Класс AppModule - точка входа в данное приложение 
-// Angular модуль - класс с декоратором NgModule
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router"; // модуль для маршрутизации
 
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
+import { AppComponent } from "./app.component";
+import { List1Component } from "./list1/list1.component";
+import { List2Component } from "./list2/list2.component";
 
-// @NgModule - декоратор, который определяет данные для создаваемого модуля.
-// Для того чтобы приложение могло выполняться в браузере, текущий модуль (корневой модуль)
-// должен выполнить импорт модуля BrowserModule взятого из @angular/platform-browser 
-// Задача BrowserModule зарегистрировать основные сервис провайдеры приложения, 
-// а также добавить общие директивы такие как ngIf и ngFor
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ], // корневой компонент данного приложения
-  bootstrap:    [ AppComponent ]  // компонент с которого начинается отображение приложения
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([ // настройка маршрутов
+            { path: "list1", component: List1Component }, // при переходе по адресу localhost:3000/component1 должен активироваться компонент List1Component
+            { path: "list2", component: List2Component },
+            { path: "", redirectTo: "list1", pathMatch: "full" }
+        ])
+    ],
+    declarations: [AppComponent, List1Component, List2Component],
+    bootstrap: [AppComponent]
 })
-
-export class AppModule { } 
+export class AppModule { }
